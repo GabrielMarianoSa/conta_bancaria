cd0bb4a576b458cc4b1acdb18d5ee6b4fa92dd05
Gabriel Mariano da Silva <gabrielmarianodasilvaa2001@hotmail.com>
2026-03-12 00:08:10 -0300

    Implementa Conta model e testes iniciais

---
+++ Colors.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Colors = void 0;
+class Colors {
+    static RESET = "\x1b[0m";
+    static BLACK = "\x1b[30m";
+    static RED = "\x1b[31m";
+    static GREEN = "\x1b[32m";
+    static YELLOW = "\x1b[33m";
+    static BLUE = "\x1b[34m";
+    static PURPLE = "\x1b[35m";
+    static CYAN = "\x1b[36m";
+    static WHITE = "\x1b[37m";
+}
+exports.Colors = Colors;
+//# sourceMappingURL=Colors.js.map
---
+++ Menu.js
@@ -0,0 +1,57 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const readlineSync = require("readline-sync");
+const Conta_1 = __importDefault(require("../model/Conta"));
+function runTests() {
+    console.log("\n--- Testes de Conta (início) ---\n");
+    const conta1 = new Conta_1.default(1, "Alice", 1000);
+    const conta2 = new Conta_1.default(2, "Bob", 500);
+    console.log("Contas criadas:");
+    console.log(conta1.toString());
+    console.log(conta2.toString());
+    console.log("\nAlice saca R$200");
+    conta1.sacar(200);
+    console.log(conta1.toString());
+    console.log("\nBob deposita R$300");
+    conta2.depositar(300);
+    console.log(conta2.toString());
+    console.log("\nAlice transfere R$100 para Bob");
+    conta1.transferir(100, conta2);
+    console.log(conta1.toString());
+    console.log(conta2.toString());
+    console.log("\n--- Testes de Conta (fim) ---\n");
+}
+function main() {
+    let opcao;
+    runTests();
+    while (true) {
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("                BANCO DO BRAZIL COM Z                ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("            1 - Criar Conta                          ");
+        console.log("            2 - Listar todas as Contas               ");
+        console.log("            3 - Buscar Conta por Número              ");
+        console.log("            4 - Atualizar Dados da Conta             ");
+        console.log("            5 - Apagar Conta                         ");
+        console.log("            6 - Sacar                                ");
+        console.log("            7 - Depositar                            ");
+        console.log("            8 - Transferir valores entre Contas      ");
+        console.log("            9 - Sair                                 ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        opcao = readlineSync.questionInt("Entre com a opcao desejada: ");
+        if (opcao === 9) {
+            console.log("\nBanco encerrado!");
+            process.exit(0);
+        }
+        console.log("\nOpcao escolhida:", opcao);
+    }
+}
+main();
+//# sourceMappingURL=Menu.js.map
